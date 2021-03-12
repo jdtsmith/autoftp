07cc50dfeef38e9cffee21de12ca1216d37102a3
JD Smith <93749+jdtsmith@users.noreply.github.com>
2021-03-12 16:22:05 -0500

    Also trap ConnectionError

--- autoftp.py
+++ autoftp.py
@@ -140,5 +140,5 @@ if __name__ == "__main__":
             if observer:
                 observer.stop()
                 observer.join()
-        except NameError:
+        except (NameError, ConnectionError):
             pass
